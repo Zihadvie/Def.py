5f078e6fd092f2424660a287b9518ccb96727075
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2026-05-09 13:03:33 +0600

    def use in python program
    #def_keyworld #def in python program

---
+++ def_keyworld.py
@@ -0,0 +1,10 @@
+def std(n,a,r):
+ print(f"Name : {n}")
+ print(f"age : {a}")
+ print(f"roll : {r}")
+ print()
+
+std("Zihad",20,85502)
+std("Mezba",19,86940)
+std("Shakil",18,89003)
+std("Razu",45,80038)

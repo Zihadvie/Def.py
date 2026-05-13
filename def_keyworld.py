b1728097256bb4a33678a5b9b407188500ba7535
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2026-05-13 11:02:49 +0600

    Refactor std function calls and print statements
    #def in and out

--- def_keyworld.py
+++ def_keyworld.py
@@ -1,10 +1,10 @@
 def std(n,a,r):
  print(f"Name : {n}")
- print(f"age : {a}")
+ print(f"age : {a}")                     #in def
  print(f"roll : {r}")
  print()
  
 std("Zihad",20,85502)
-std("Mezba",19,86940)
+std("Mezba",19,86940)                    #out def
 std("Shakil",18,89003)
 std("Razu",45,80038)
